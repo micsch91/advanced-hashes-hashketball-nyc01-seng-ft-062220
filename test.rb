2dba22ecfb38c12ad8dac5ebd7d55d8f707b8de5
Michael Schneider <mic.sch91@gmail.com>
2020-06-11 20:55:14 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -42,7 +42,7 @@ def game_hash
         {
           player_name: "Mason Plumlee",
           number: 1,
-          shoe: 19,
+          shoe: 9,
           points: 26,
           rebounds: 11,
           assists: 6,
